41e01825aad7103d336da8a4c80dbeb8db962709
yarden.regev <yardisome@gmail.com>
2022-08-28 15:12:37 +0300

    made a basic api for getting data of a stock

--- market_simulator/back-end/include/finance_api.py
+++ market_simulator/back-end/include/finance_api.py
@@ -1,5 +1,25 @@
 import yfinance as yf
 
-def GetStockInfo(symbol):
-    return yf.Ticker(symbol).info
+def GetStock(symbol):
+    return yf.Ticker(symbol)
 
+def GetStockInfo(stock):
+    return stock.info
+
+def GetStockMarketPrice(stock):
+    return stock.info["regularMarketPrice"]
+
+def GetStockSector(stock):
+    return stock.info["sector"]
+
+def GetLongBussinessSummary(stock):
+    return stock.info["longBusinessSummary"]
+
+def GetStockShortName(stock):
+    return stock.info["shortName"]
+
+def GetStockLongName(stock):
+    return stock.info["longName"]
+
+def GetStockLogoUrl(stock):
+    return stock.info["logo_url"]
--- market_simulator/back-end/include/test.py
+++ market_simulator/back-end/include/test.py
@@ -1,5 +1,12 @@
-from finance_api import GetStockInfo
+import json
+from finance_api import *
 
 if __name__ == "__main__":
-    info = GetStockInfo("MSFT")
-    print(info)
+    msft = GetStock("MSFT")
+    stock_profile = {}
+    stock_profile["name"] = GetStockShortName(msft)
+    stock_profile["market price"] = GetStockMarketPrice(msft)
+    stock_profile["sector"] = GetStockSector(msft)
+    stock_profile["summary"] = GetLongBussinessSummary(msft)
+
+    print(json.dumps(stock_profile, indent=4))
