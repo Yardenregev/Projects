043a0b5c8b5628e5a07ce5d1e0a7e8abbe4a934b
yarden.regev <excerpta.app@gmail.com>
2024-05-28 20:29:37 +0300

    removed the market simulator, was unfinished project

--- market_simulator/front-end/my_app/src/components/general_components/js/navbar_items.js
+++
@@ -1,8 +0,0 @@
-const NavbarItems = [
-    {id: 1, name: "Protfolio", to:"/"},
-    {id: 2, name: "Research & Trade", to:"/research&trade"},
-    {id: 3, name: "Log in"},
-    
-];
-
-export default NavbarItems;
